f19e1abcd2be4d74e6fa6267c60b377fd16726c3
Mohamed Hafid <mohamedhafid825@gmail.com>
2025-06-11 22:45:56 +0530

    small modification

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
         <div className="absolute inset-0 bg-black opacity-80 rounded-xl">
           <div className="flex flex-col items-center justify-center h-full px-4 sm:px-6 md:px-10 lg:px-16">
             <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-white text-center">
-              Discover Your Next Favorite <br />
+              Discover Your Favorite Item <br />
               <span>Product</span>
             </h1>
             <p className="mt-4 text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-300 text-center">
